02e46b4974a86c7461eb92a759239fc52938ee1d
tukaisutegmks-sys <tukaisutegmks@gmail.com>
2026-07-24 17:16:25 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   build: {
-    manifest: true,
+    manifest: 'manifest.json',
     rollupOptions: {
       input: ['/client-entry.tsx'],
     },
